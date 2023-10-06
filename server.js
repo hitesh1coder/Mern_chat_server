97a9d5089d7a8e0b8ab2ea216b96f4fdf1cdf5aa
codewitHK1 <kumarhk1002@gmail.com>
2023-10-06 11:01:27 +0530

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./Routes/userRoutes.js";
+import cors from "cors";
 
 import messageRoutes from "./Routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -24,6 +25,7 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
