ddfdfa04e3f8e47d2b75eb35025b2bfdb93c6132
codewitHK1 <kumarhk1002@gmail.com>
2023-10-06 10:09:45 +0530

    cors updated

--- socket/socket.js
+++ socket/socket.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `${process.env.CLIENT_PORT}`,
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
